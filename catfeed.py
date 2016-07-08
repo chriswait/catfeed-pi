9feaa6cb16e2bd463d2ef35d632fc23c1db45a98
chriswait <chriswait91@gmail.com>
2016-07-08 14:19:46 +0000

    removes constants, adds return code checking, decrease frequency

--- catfeed.py
+++ catfeed.py
@@ -6,23 +6,27 @@ from requests import get
 from json import dumps
 
 service_root = "http://cat.chriswait.net/"
+check_url = service_root
+add_feed_url = service_root + "add_feed"
+check_frequency_seconds = 60
+button_pin = 25
+led_pin = 14
 
-button = Button(25)
-led = LED(14)
-led.off()
+button = Button(button_pin)
+led = LED(led_pin)
 
 def add_feed():
-    url = service_root + "add_feed"
-    req = get(url)
+    req = get(add_feed_url)
 button.when_pressed = add_feed
 
 def check_should_feed():
-    url = service_root
-    req = get(url)
-    return (req.text=="1")
+    req = get(check_url)
+    if (req.status_code == 200):
+        return (req.text == "1")
+    return False
 
 while True:
-    sleep(10)
+    sleep(check_frequency_seconds)
     if check_should_feed():
         led.on()
     else:
